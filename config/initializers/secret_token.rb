9b9230cb32f45044eb14b4f152293ac5796fc481
Pieter Moorman <pietermoorman@hotmail.com>
2013-05-01 21:46:23 +0200

    Add User model with Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Education::Application.config.secret_token = '3d2f809f87c7675b637fea4b40081cfecdf711e67dc6c58b778fc689cc82a9aa724be6eb5694d1de2557d30a28ea6e21a141aa43da1fa9bad3ad48b6c2c4616b'
